df0f07c158040393d4a059c9e3b30586371bad80
landrea-velez <landrea.velez@gmail.com>
2022-02-03 08:24:28 -0500

    [add] componentes Angular

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaComponent,
+    CompbComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule
